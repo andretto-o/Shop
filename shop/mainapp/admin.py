8d9a304de4a2e9602428dd85f3c1362c5d1bcff8
Work <progame.az@gmail.com>
2021-05-24 12:44:01 +0300

    9 этапов закончено. Изображение ImageField .

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,11 +1,33 @@
-from django.forms import ModelChoiceField, ModelForm
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django.contrib import admin
-
 from .models import *
+
+from PIL import Image
 
 
 # фильтрация списка категорий при создании товара
+class NotebookAdminFrom(ModelForm):
+    MIN_RESOLUTION = (4000, 4000)
+    MAX_RESOLUTION =(4000, 4000)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Загружайте изображения с минимальным разрешением {}x{}'.format(
+            *self.MIN_RESOLUTION
+        )
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Разрешение меньше минимального')
+        return image
+
+
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminFrom
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
